5b52dc58781b3298e89335b52da478229d5734a9
inwoo <jiw413@naver.com>
2024-11-03 21:16:24 +0900

    update: 유저 패스워드 정규표현식에 특수문자 조건 추가

--- src/main/kotlin/com/namanmoo/kotlinboard/service/dto/UserDto.kt
+++ src/main/kotlin/com/namanmoo/kotlinboard/service/dto/UserDto.kt
@@ -27,8 +27,8 @@ class UserDto {
 
         @JsonProperty("password")
         @field:Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,15}\$",
-            message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9)"
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])[a-zA-Z0-9[^a-zA-Z0-9]]{8,15}\$",
+            message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자"
         )
         private val _password: String?
     ): Serializable {
